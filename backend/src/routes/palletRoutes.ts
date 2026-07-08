dcbe4c55ab078219ce2c07ee4dcf113a7b2ad9fe
unknown <raoanmarceljukowski@gmail.com>
2026-07-08 09:38:06 -0300

    sistema de autenticação de rotas

--- backend/src/routes/palletRoutes.ts
+++ backend/src/routes/palletRoutes.ts
@@ -1,23 +1,29 @@
 import { Router } from 'express';
-import { criarPallet, buscarPalletPorId, listarPallets, biparItem, transferirUm, transferirEmLote, enviarParaRMA, excluirPallet} from '../controllers/palletController.js';
+import { 
+  criarPallet, buscarPalletPorId, listarPallets, biparItem, 
+  transferirUm, transferirEmLote, enviarParaRMA, excluirPallet
+} from '../controllers/palletController.js';
 import { exportarHistoricoExcel, exportarRelatorioRMA } from '../controllers/excelController.js';
+
+// Importe o seu middleware (lembre-se do .js no final se o seu projeto estiver usando ES Modules)
+import { autenticarToken } from '../middlewares/authMiddleware.js'; 
 
 const router = Router();
 
-// Rotas de Gestão do Pallet
-router.post('/pallets', criarPallet);
-router.get('/pallets', listarPallets);
-router.get('/pallets/:id', buscarPalletPorId);
-router.get('/historico/exportar-rma', exportarRelatorioRMA);
+// Rotas de Gestão do Pallet (Todas protegidas)
+router.post('/pallets', autenticarToken, criarPallet);
+router.get('/pallets', autenticarToken, listarPallets);
+router.get('/pallets/:id', autenticarToken, buscarPalletPorId);
 
-// Rota Operacional (O Bip do leitor)
-router.post('/pallets/bipar', biparItem);
-router.post('/historico/exportar', exportarHistoricoExcel);
-router.post('/pallets/enviar-rma', enviarParaRMA);
+// Rotas de Excel (Protegidas)
+router.get('/historico/exportar-rma', autenticarToken, exportarRelatorioRMA);
+router.post('/historico/exportar', autenticarToken, exportarHistoricoExcel);
 
-router.put('/pallets/transferir', transferirUm);
-router.put('/pallets/transferir-lote', transferirEmLote);
-
-router.delete('/pallets/:id', excluirPallet);
+// Rotas Operacionais (Protegidas)
+router.post('/pallets/bipar', autenticarToken, biparItem);
+router.post('/pallets/enviar-rma', autenticarToken, enviarParaRMA);
+router.put('/pallets/transferir', autenticarToken, transferirUm);
+router.put('/pallets/transferir-lote', autenticarToken, transferirEmLote);
+router.delete('/pallets/:id', autenticarToken, excluirPallet);
 
 export default router;
